d9363ae0d0d2c37de1833c9d06ec6155181579dd
Aluno <gabriel.lisboa.141@gmail.com>
2024-10-17 09:00:45 -0300

    implementando requisição para listar pacientes

--- src/main/java/edu/ifpr/receitafacil/controllers/PacienteController.java
+++ src/main/java/edu/ifpr/receitafacil/controllers/PacienteController.java
@@ -1,5 +1,42 @@
 package edu.ifpr.receitafacil.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.ifpr.receitafacil.model.Paciente;
+import edu.ifpr.receitafacil.services.PacienteService;
+
+@RestController
+@RequestMapping("/paciente")
 public class PacienteController {
+
+    @Autowired
+    private PacienteService pacienteService;
+
+    public PacienteController(PacienteService pacienteService) {
+        super();
+        this.pacienteService = pacienteService;
+    }
     
+    @GetMapping("/listar")
+    public List<Paciente> findAll() {
+        return pacienteService.findAllPacientes();
+    }
+
+    @PutMapping("/salvar")
+    public String salvar() {
+        return "paciente/salvar";
+    }
+
+    @DeleteMapping("/excluir")
+    public String excluir() {
+        return "paciente/excluir";
+    }
+
 }
--- src/main/java/edu/ifpr/receitafacil/repository/ReceitaRepository.java
+++ src/main/java/edu/ifpr/receitafacil/repository/ReceitaRepository.java
@@ -1,6 +1,7 @@
 package edu.ifpr.receitafacil.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import edu.ifpr.receitafacil.model.Receita;
@@ -8,4 +9,6 @@ import edu.ifpr.receitafacil.model.Receita;
 @Repository
 public interface ReceitaRepository extends JpaRepository<Receita, Integer> {
   
+    @Query("SELECT r FROM Receita r WHERE r.paciente.id = :idPaciente")
+    public Receita findByPacienteId(Integer idPaciente);
 }
