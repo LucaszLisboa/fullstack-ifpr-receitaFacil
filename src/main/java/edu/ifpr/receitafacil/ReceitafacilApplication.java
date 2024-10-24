fd9b5dadb8591dd6a34a3986bd9ed124751e5edd
Aluno <gabriel.lisboa.141@gmail.com>
2024-10-24 09:29:24 -0300

    adicionando documentação com swagger

--- src/main/java/edu/ifpr/receitafacil/ReceitafacilApplication.java
+++ src/main/java/edu/ifpr/receitafacil/ReceitafacilApplication.java
@@ -3,7 +3,7 @@ package edu.ifpr.receitafacil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class ReceitafacilApplication {
 
 	public static void main(String[] args) {
